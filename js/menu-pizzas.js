31c0f6ae03cadaede1f1ad428c9cbd606ed65f11
Caio589 <caiosan321@icloud.com>
2026-01-20 10:18:25 -0300

    Create pizza menu with 21 flavors and prices
    Added a menu for Danburger's pizzas with 21 flavors and their prices.

---
+++ js/menu-pizzas.js
@@ -0,0 +1,97 @@
+/* ===============================
+   DANBURGER'S - PIZZAS
+   21 sabores | P / M / G
+================================ */
+
+const MENU_PIZZAS = [
+  {
+    name: "Calabresa",
+    desc: "Molho, calabresa, cebola e orégano",
+    P: 22.00, M: 28.00, G: 35.00
+  },
+  {
+    name: "Mussarela",
+    desc: "Molho, mussarela, tomate e orégano",
+    P: 24.00, M: 31.00, G: 37.00
+  },
+  {
+    name: "Portuguesa",
+    desc: "Molho, mussarela, ervilha, presunto, milho, ovo e orégano",
+    P: 29.00, M: 32.00, G: 39.00
+  },
+  {
+    name: "Frango Catupiry",
+    desc: "Molho, frango desfiado, catupiry e orégano",
+    P: 28.00, M: 31.00, G: 38.00
+  },
+  {
+    name: "Bacon",
+    desc: "Molho, mussarela, bacon e orégano",
+    P: 29.00, M: 32.00, G: 41.00
+  },
+  {
+    name: "Marguerita",
+    desc: "Molho, mussarela, manjericão e orégano",
+    P: 24.00, M: 31.00, G: 37.00
+  },
+  {
+    name: "Brócolis",
+    desc: "Molho, brócolis, mussarela, alho frito e orégano",
+    P: 28.00, M: 32.00, G: 37.00
+  },
+  {
+    name: "Atum",
+    desc: "Molho, atum, mussarela, cebola e orégano",
+    P: 29.00, M: 33.00, G: 40.00
+  },
+  {
+    name: "Toscana",
+    desc: "Molho, calabresa, queijo, cebola e orégano",
+    P: 29.00, M: 33.00, G: 39.00
+  },
+  {
+    name: "Toscana Bacon",
+    desc: "Molho, calabresa, queijo, bacon, cebola e orégano",
+    P: 32.00, M: 36.00, G: 45.00
+  },
+  {
+    name: "Carne Seca",
+    desc: "Molho, carne seca, mussarela, cebola e catupiry",
+    P: 32.00, M: 37.00, G: 45.00
+  },
+  {
+    name: "4 Queijos",
+    desc: "Molho, mussarela, catupiry, cheddar e parmesão",
+    P: 29.00, M: 31.00, G: 44.00
+  },
+  {
+    name: "Milho",
+    desc: "Molho, mussarela, milho e orégano",
+    P: 24.00, M: 30.00, G: 37.00
+  },
+  {
+    name: "Baiana",
+    desc: "Molho, presunto, calabresa, ovo, pimenta e orégano",
+    P: 31.00, M: 36.00, G: 43.00
+  },
+  {
+    name: "Nordestina",
+    desc: "Molho, mussarela, carne seca, banana da terra e orégano",
+    P: 32.00, M: 37.00, G: 45.00
+  },
+  {
+    name: "Danburgers",
+    desc: "Molho, mussarela, carne seca, barbecue e orégano",
+    P: 32.00, M: 37.00, G: 45.00
+  },
+  {
+    name: "Danburger’s GBI",
+    desc: "Molho, frango, catupiry original e orégano",
+    P: 33.00, M: 43.00, G: 53.00
+  },
+  {
+    name: "Danburgers VCA",
+    desc: "Molho, frango, queijo, milho, bacon e orégano",
+    P: 32.00, M: 37.00, G: 45.00
+  }
+];

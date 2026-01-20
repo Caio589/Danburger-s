b65c58db19d03383133e11ebfab155278fbc6bfc
Caio589 <caiosan321@icloud.com>
2026-01-20 10:25:33 -0300

    Add menu-parmegiana.js with parmegiana items

---
+++ js/js/js/js/menu-parmegiana.js
@@ -0,0 +1,17 @@
+/* =====================================
+   DANBURGER'S - PARMEGIANA (2 ITENS)
+   Cardápio fiel ao físico
+===================================== */
+
+const MENU_PARMEGIANA = [
+  {
+    name: "Parmegiana de Filé Mignon",
+    price: 35.00,
+    desc: "Filé mignon à parmegiana acompanhado de fritas"
+  },
+  {
+    name: "Parmegiana de Frango",
+    price: 33.00,
+    desc: "Filé de frango à parmegiana acompanhado de fritas"
+  }
+];
